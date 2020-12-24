410b3bf4b6cf372d855aa4a02e17b182358c90f2
ss2400 <sbledl@hotmail.com>
2020-12-24 13:44:05 -0800

    Add files via upload

---
+++ 2000kg_scale/go.bat
@@ -0,0 +1,9 @@
+set OPENSCADPATH=D:\openscad-2019.05
+
+set PYTHONPATH=D:\openscad-2019.05\libraries\NopSCADlib\scripts
+
+set PATH=%PATH%;D:\openscad-2019.05\libraries\NopSCADlib\scripts;D:\openscad-2019.05
+
+python ..\..\scripts\make_all.py
+
+pause
